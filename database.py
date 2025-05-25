bdaac4930008cd0a6e5af3cafced766b14f64d30
Sosiggg <salas.ivisusej@gmail.com>
2025-05-25 19:47:22 +0800

    Update database configuration to use PostgreSQL connection string

--- database.py
+++ database.py
@@ -1,10 +1,8 @@
 # database.py
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Read DATABASE_URL from environment variable
-database_url = os.environ.get("DATABASE_URL")
+database_url = "postgresql://envirosensedb_user:rkNngiLgzhr6AQlLiywstBPl4gARlKZ5@dpg-d0kapsjuibrs739chbvg-a.singapore-postgres.render.com/envirosensedb"
 
 engine = create_engine(database_url, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
